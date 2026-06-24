5e04bf636317e77d7921d29fa2dd7fd6c944b4a7
khodorhasan21 <khodorhasan17@gmail.com>
2026-06-24 05:23:39 +0300

    Add vercel.json

--- next.config.js
+++ next.config.js
@@ -15,21 +15,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/fonts/:filename",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, HEAD, OPTIONS" },
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
